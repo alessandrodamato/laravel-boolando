099884354c4dd0ec079a8502001b60f6d2e55571
alessandrodamato <alessandro042004@gmail.com>
2024-05-09 17:43:48 +0200

    [add] badges, discount calc

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,20 +10,29 @@
           <img class="main-img" src="{{Vite::asset('resources/img/')}}{{$product->frontImage}}" alt="{{$product->name}}">
           <img class="hover-img" src="{{Vite::asset('resources/img/')}}{{$product->backImage}}" alt="{{$product->name}}">
           <div class="f-size-small tags">
-            @foreach ($product->badges as $badge)
+            @foreach (array_reverse($product->badges) as $badge)
                 <div class="badge {{$badge['type']}}">{{$badge['value']}}</div>
             @endforeach
           </div>
-          <div class="favourite">&hearts;</div>
+          <div class="favourite {{$product->isInFavorites ? 'active' : ''}}">&hearts;</div>
         </div>
 
         <div class="f-size-small product-brand">{{$product->brand}}</div>
 
         <div class="product-name">{{$product->name}}</div>
 
-        <span class="f-size-small lower-price">{{$product->price}} &euro;</span>
-
         <span class="f-size-small price">{{$product->price}} &euro;</span>
+
+        <span class="f-size-small lower-price">
+            @foreach($product->badges as $badge)
+                @if ($badge['type'] === 'discount')
+                    @if ($product->price *= 1 - (str_replace('-', '', str_replace('%', '', $badge['value'])) / 100) != 0)
+                        {{number_format($product->price *= 1 - (str_replace('-', '', str_replace('%', '', $badge['value'])) / 100), 2)}} &euro;
+                    @endif
+                @endif
+            @endforeach
+        </span>
+
 
     </div>
     @endforeach
